019bd4451031b4cf096912f27ffac5f987813f4e
Neta Hadar Itzhak <netook6@gmail.com>
2021-10-07 16:06:34 +0300

    refactor: add index.js and merge feature.html to index.html

---
+++ index.js
@@ -0,0 +1,7 @@
+const mainButton = document.querySelector(".main__button");
+const cards = document.querySelector(".cards");
+
+mainButton.addEventListener("click", () => {
+    cards.classList.add("cards_active");
+    window.scrollTo(0,document.querySelector(".main").scrollHeight);
+});
