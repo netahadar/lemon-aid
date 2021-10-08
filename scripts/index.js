fc6e7c75fcf03b7230fa6f940ea665c9cb4ac1fd
Sarah Edison <edison.sarahc@gmail.com>
2021-10-08 08:37:04 -0400

    remove blank

--- scripts/index.js
+++ scripts/index.js
@@ -9,5 +9,5 @@ mainButton.addEventListener("click", () => {
 const helpCard = document.querySelector(".cards__paragraph_place_second");
 
 helpCard.addEventListener("click", () => {
- open("https://edisonsc.github.io/lemon-aid/support.html", "_blank")
+ open("https://edisonsc.github.io/lemon-aid/support.html")
 })
