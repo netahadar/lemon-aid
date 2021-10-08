ebde648f2ddc154685003af21dd79331a3a7307a
Sarah Edison <edison.sarahc@gmail.com>
2021-10-07 22:10:10 -0400

    deletion

--- index.js
+++
@@ -1,7 +0,0 @@
-const mainButton = document.querySelector(".main__button");
-const cards = document.querySelector(".cards");
-
-mainButton.addEventListener("click", () => {
-    cards.classList.add("cards_active");
-    window.scrollTo(0,document.querySelector(".main").scrollHeight);
-});
