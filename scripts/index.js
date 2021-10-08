17b416829b68f2afcde83872409475443dc03ece
Sarah Edison <edison.sarahc@gmail.com>
2021-10-07 22:27:58 -0400

    add function to open support page on button click

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,10 @@ mainButton.addEventListener("click", () => {
     cards.classList.add("cards_active");
     window.scrollTo(0,document.querySelector(".main").scrollHeight);
 });
+
+const helpCard = document.querySelector(".cards__paragraph_place_second");
+
+helpCard.addEventListener("click", () => {
+ open("../support.html", "_blank")
+})
+
