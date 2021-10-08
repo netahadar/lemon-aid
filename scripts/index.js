efbe3fd0613009cc0c7af06da288e3bd9edb3723
Sarah Edison <edison.sarahc@gmail.com>
2021-10-08 08:34:46 -0400

    fix: move javascript file back to index.js

--- scripts/index.js
+++ scripts/index.js
@@ -6,4 +6,8 @@ mainButton.addEventListener("click", () => {
     window.scrollTo(0,document.querySelector(".main").scrollHeight);
 });
 
+const helpCard = document.querySelector(".cards__paragraph_place_second");
 
+helpCard.addEventListener("click", () => {
+ open("https://edisonsc.github.io/lemon-aid/support.html", "_blank")
+})
--- scripts/support.js
+++
@@ -1,5 +0,0 @@
-const helpCard = document.querySelector(".cards__paragraph_place_second");
-
-helpCard.addEventListener("click", () => {
- open("https://edisonsc.github.io/lemon-aid/support.html", "_blank")
-})
